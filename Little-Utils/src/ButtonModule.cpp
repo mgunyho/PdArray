c1e46929a865f3cfd570781ca235154a1c48ff73
Marton Gunyho <andras.gunyho@aalto.fi>
2018-12-22 09:42:58 +0200

    Finalize layout.

--- Little-Utils/src/ButtonModule.cpp
+++ Little-Utils/src/ButtonModule.cpp
@@ -29,8 +29,6 @@ struct ButtonModule : Module {
 		CONST_5_LIGHTM,
 		CONST_10_LIGHTP,
 		CONST_10_LIGHTM,
-		//CONST_SIGN_LIGHTP,
-		//CONST_SIGN_LIGHTM,
 		NUM_LIGHTS
 	};
 
@@ -101,48 +99,16 @@ void ButtonModule::step() {
 		}
 	}
 
-	if(const_choice < 3) {
-		//lights[CONST_SIGN_LIGHTP].setBrightnessSmooth(1.f);
-		//lights[CONST_SIGN_LIGHTM].setBrightnessSmooth(0.f);
-	} else {
+	if(const_choice >= 3) {
 		outputs[CONST_OUTPUT].value *= -1;
-		//lights[CONST_SIGN_LIGHTP].setBrightnessSmooth(0.f);
-		//lights[CONST_SIGN_LIGHTM].setBrightnessSmooth(1.f);
 	}
+
 }
-
-
-//struct ButtonWidget : SVGButton {
-	//ButtonModule *module = NULL;
-
-	////template <typename T = ButtonWidget>
-	////static T *create(Vec pos, Module *module)
-	////onAction()
-
-	//ButtonWidget() {
-	//	//TODO: own button images here
-	//	setSVGs(SVG::load(assetGlobal("res/ComponentLibrary/BefacoPush_0.svg")),
-	//			SVG::load(assetGlobal("res/ComponentLibrary/BefacoPush_1.svg")));
-	//}
-
-	//template <typename T = ButtonWidget>
-	//static T* create(Vec pos, ButtonModule *module) {
-	//	ButtonWidget *o = Widget::create<ButtonWidget>(pos);
-	//	o->module = module;
-	//	return o;
-	//}
-
-	//void onAction(EventAction &e) override {
-	//	module->triggerGenerator.trigger(1e-3f);
-	//}
-//};
 
 struct ButtonWidget : SVGSwitch, MomentarySwitch {
 	ButtonWidget() {
 		addFrame(SVG::load(assetPlugin(plugin, "res/Button_button_0.svg")));
 		addFrame(SVG::load(assetPlugin(plugin, "res/Button_button_1.svg")));
-		//addFrame(SVG::load(assetGlobal("res/ComponentLibrary/CDK_0.svg")));
-		//addFrame(SVG::load(assetGlobal("res/ComponentLibrary/CDK_1.svg")));
 	}
 };
 
@@ -153,41 +119,24 @@ struct ButtonModuleWidget : ModuleWidget {
 		addChild(Widget::create<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
 		addChild(Widget::create<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		//addParam(ParamWidget::create<TL1105>(Vec(10, 30), module, ButtonModule::BUTTON_PARAM, 0.0f, 1.0f, 0.0f));
-		//addParam(ParamWidget::create<Davies1900hBlackKnob>(Vec(28, 87), module, ButtonModule::PITCH_PARAM, -3.0, 3.0, 0.0));
 		addChild(ParamWidget::create<ButtonWidget>(Vec(7.5, 7.5 + RACK_GRID_WIDTH), module, ButtonModule::BUTTON_PARAM, 0.0f, 1.0f, 0.0f));
-		//addChild(createParamCentered<TL1105>(Vec(30, 100), module, ButtonModule::BUTTON_PARAM, 0.0f, 1.0f, 0.0f));
-		//addChild(Widget::create<BefacoPush>(Vec(10, 30)));
-		//addChild(Widget::create<ButtonWidget>(Vec(10, 30), module));
 
-		//addInput(Port::create<PJ301MPort>(Vec(33, 186), Port::INPUT, module, ButtonModule::PITCH_INPUT));
+		addInput(createInputCentered<PJ301MPort>(Vec(22.5, 87), module, ButtonModule::TRIG_INPUT));
 
-		//addOutput(Port::create<PJ301MPort>(Vec(33, 275), Port::OUTPUT, module, ButtonModule::SINE_OUTPUT));
-		//addOutput(Port::create<PJ301MPort>(Vec(18, 217), Port::OUTPUT, module, ButtonModule::TRIG_OUTPUT));
-		addInput(createInputCentered<PJ301MPort>(Vec(22.5, 165), module, ButtonModule::TRIG_INPUT));
-		addOutput(createOutputCentered<PJ301MPort>(Vec(22.5, 215), module, ButtonModule::TRIG_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(Vec(22.5, 265), module, ButtonModule::GATE_OUTPUT));
-		addOutput(createOutputCentered<PJ301MPort>(Vec(22.5, 315), module, ButtonModule::TOGGLE_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(Vec(22.5, 142), module, ButtonModule::TRIG_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(Vec(22.5, 192), module, ButtonModule::GATE_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(Vec(22.5, 242), module, ButtonModule::TOGGLE_OUTPUT));
 
-		//addChild(ModuleLightWidget::create<MediumLight<RedLight>>(Vec(41, 59), module, ButtonModule::TRIG_LIGHT));
 		double offset = 3.6;
-		addChild(createLightCentered<TinyLight<GreenLight>>(Vec(37.5 - offset, 200 + offset), module, ButtonModule::TRIG_LIGHT));
-		addChild(createLightCentered<TinyLight<GreenLight>>(Vec(37.5 - offset, 250 + offset), module, ButtonModule::GATE_LIGHT));
-		addChild(createLightCentered<TinyLight<GreenLight>>(Vec(37.5 - offset, 300 + offset), module, ButtonModule::TOGGLE_LIGHT));
+		addChild(createLightCentered<TinyLight<GreenLight>>(Vec(37.5 - offset, 127 + offset), module, ButtonModule::TRIG_LIGHT));
+		addChild(createLightCentered<TinyLight<GreenLight>>(Vec(37.5 - offset, 177 + offset), module, ButtonModule::GATE_LIGHT));
+		addChild(createLightCentered<TinyLight<GreenLight>>(Vec(37.5 - offset, 227 + offset), module, ButtonModule::TOGGLE_LIGHT));
 
-		addOutput(createOutputCentered<PJ301MPort>(Vec(22.5, 115), module, ButtonModule::CONST_OUTPUT));
+		addOutput(createOutputCentered<PJ301MPort>(Vec(22.5, 320), module, ButtonModule::CONST_OUTPUT));
 
-		//addChild(createLight<SmallLight<GreenLight>>(Vec(10.5, 78), module, ButtonModule::CONST_1_LIGHT));
-		addChild(createLightCentered<SmallLight<GreenRedLight>>(Vec(15, 76), module, ButtonModule::CONST_1_LIGHTP));
-		addChild(createLightCentered<SmallLight<GreenRedLight>>(Vec(15, 86), module, ButtonModule::CONST_5_LIGHTP));
-		addChild(createLightCentered<SmallLight<GreenRedLight>>(Vec(15, 96), module, ButtonModule::CONST_10_LIGHTP));
-		//addChild(ModuleLightWidget::create<TinyLight<GreenRedLight>>(Vec(23.5, 89), module,
-		//			ButtonModule::CONST_SIGN_LIGHTP
-		//			//ButtonModule::CONST_SIGN_LIGHTM
-		//			));
-
-		//addParam(
-
+		addChild(createLightCentered<SmallLight<GreenRedLight>>(Vec(15, 281), module, ButtonModule::CONST_1_LIGHTP));
+		addChild(createLightCentered<SmallLight<GreenRedLight>>(Vec(15, 291), module, ButtonModule::CONST_5_LIGHTP));
+		addChild(createLightCentered<SmallLight<GreenRedLight>>(Vec(15, 301), module, ButtonModule::CONST_10_LIGHTP));
 	}
 };
 
