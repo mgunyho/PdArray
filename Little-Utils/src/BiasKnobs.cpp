af4ddbddf32715a027f8afde9f719fbc09fd19cf
Marton Gunyho <andras.gunyho@aalto.fi>
2018-12-26 00:41:13 +0200

    Add multiple elements.

--- Little-Utils/src/BiasKnobs.cpp
+++ Little-Utils/src/BiasKnobs.cpp
@@ -4,34 +4,34 @@
 
 #include <algorithm> // std::replace
 
-//const int N_KNOBS = 6;
+const int N_KNOBS = 5;
 
 //TODO: good name?
 struct BiasKnobs : Module {
 	enum ParamIds {
 		BIAS_1_PARAM,
-		//BIAS_2_PARAM,
-		//BIAS_3_PARAM,
-		//BIAS_4_PARAM,
-		//BIAS_5_PARAM,
+		BIAS_2_PARAM,
+		BIAS_3_PARAM,
+		BIAS_4_PARAM,
+		BIAS_5_PARAM,
 		//BIAS_6_PARAM,
 		NUM_PARAMS
 	};
 	enum InputIds {
 		INPUT_1,
-		//INPUT_2,
-		//INPUT_3,
-		//INPUT_4,
-		//INPUT_5,
+		INPUT_2,
+		INPUT_3,
+		INPUT_4,
+		INPUT_5,
 		//INPUT_6,
 		NUM_INPUTS
 	};
 	enum OutputIds {
 		OUTPUT_1,
-		//OUTPUT_2,
-		//OUTPUT_3,
-		//OUTPUT_4,
-		//OUTPUT_5,
+		OUTPUT_2,
+		OUTPUT_3,
+		OUTPUT_4,
+		OUTPUT_5,
 		//OUTPUT_6,
 		NUM_OUTPUTS
 	};
@@ -61,8 +61,10 @@ struct BiasKnobs : Module {
 
 void BiasKnobs::step() {
 
-	float bias = params[BIAS_1_PARAM].value;
-	outputs[OUTPUT_1].value = inputs[INPUT_1].value + bias;
+	for(int i = 0; i < N_KNOBS; i++) {
+		float bias = params[BIAS_1_PARAM + i].value;
+		outputs[OUTPUT_1 + i].value = inputs[INPUT_1 + i].value + bias;
+	}
 
 	//lights[GATE_LIGHT].setBrightnessSmooth(outputs[GATE_OUTPUT].value);
 
@@ -70,7 +72,7 @@ void BiasKnobs::step() {
 
 struct BiasKnobsWidget : ModuleWidget {
 	BiasKnobs *module;
-	TextBox *display;
+	TextBox *displays[N_KNOBS];
 
 	BiasKnobsWidget(BiasKnobs *module) : ModuleWidget(module) {
 		this->module = module;
@@ -81,36 +83,42 @@ struct BiasKnobsWidget : ModuleWidget {
 		addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 2*RACK_GRID_WIDTH, 0)));
 		addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 2*RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		addParam(createParamCentered<Trimpot>(Vec(22.5, 37.5), module,
-					BiasKnobs::BIAS_1_PARAM, -10.f, 10.f, 0.f));
-		//addParam(createParamCentered<Trimpot>(
-		//			Vec(22.5, 133), module, PulseGenModule::CV_AMT_PARAM,
-		//			-1.f, 1.f, 0.f));
-		//addParam(createParam<CKSS>(Vec(7.5, 69), module, PulseGenModule::LIN_LOG_MODE_PARAM, 0.f, 1.f, 1.f));
+		float elem_height = 65.f;
+		for(int i = 0; i < N_KNOBS; i++) {
+			float top_items_center_y = 30.f + i * elem_height;
+			float bot_items_center_y = 60.f + i * elem_height;
+			addParam(createParamCentered<Trimpot>(Vec(20., top_items_center_y),
+						module, BiasKnobs::BIAS_1_PARAM + i, -10.f, 10.f, 0.f));
+			addInput(createInputCentered<PJ301MPort>(Vec(20., bot_items_center_y),
+						module, BiasKnobs::INPUT_1 + i));
+			//addOutput(createOutputCentered<PJ301MPort>(Vec(120 - 22.5, bot_items_center_y),
+			//			module, BiasKnobs::OUTPUT_1 + i));
+			addOutput(createOutputCentered<PJ301MPort>(Vec(75 - 20., bot_items_center_y),
+						module, BiasKnobs::OUTPUT_1 + i));
 
-		addInput(createInputCentered<PJ301MPort>(Vec(22.5, 67.5), module, BiasKnobs::INPUT_1));
-		//addOutput(createOutputCentered<PJ301MPort>(Vec(22.5, 278), module, BiasKnobs::OUTPUT_1));
-		addOutput(createOutputCentered<PJ301MPort>(Vec(90 - 22.5, 67.5), module, BiasKnobs::OUTPUT_1));
-
-		//double offset = 3.6;
-		//addChild(createLightCentered<TinyLight<GreenLight>>(Vec(37.5 - offset, 263 + offset), module, PulseGenModule::GATE_LIGHT));
-
-		//TODO: is 'new' good? does it get freed somewhere?
-		display = new TextBox();
-		display->box.pos = Vec(37.5, 37.5 - display->box.size.y * 0.5f);
-		display->font_size = 18;
-		display->box.size.x = 45;
-		display->textOffset.x = display->box.size.x * 0.5f;
-		addChild(display);
+			//TODO: is 'new' good? does it get freed somewhere?
+			TextBox *display = new TextBox();
+			display->box.pos = Vec(35.0, top_items_center_y - display->box.size.y * 0.5f);
+			//display->font_size = 18;
+			//display->box.size.x = 45;
+			display->font_size = 14;
+			display->box.size.x = 35;
+			display->textOffset.x = display->box.size.x * 0.5f;
+			displays[i] = display;
+			addChild(displays[i]);
+		}
 
 	}
 
 	void step() override {
 		ModuleWidget::step();
-		float bias = module->params[BiasKnobs::BIAS_1_PARAM].value;
-		std::string s = stringf(fabs(bias) < 9.95f ? "%+.1fV" : "%+.0f.V", bias);
-		std::replace(s.begin(), s.end(), '0', 'O');
-		display->setText(s);
+		for(int i = 0; i < N_KNOBS; i++) {
+			float bias = module->params[BiasKnobs::BIAS_1_PARAM + i].value;
+			std::string s = stringf(fabs(bias) < 9.95f ? "%+.1fV" : "%+.0f.V", bias);
+			//std::string s = stringf(fabs(bias) < 9.95f ? "%+.1f" : "%+.0f.", bias);
+			std::replace(s.begin(), s.end(), '0', 'O');
+			displays[i]->setText(s);
+		}
 	}
 };
 
