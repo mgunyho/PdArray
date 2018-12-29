1b3c6e7898265068b2757f99b6188da16914c430
Marton Gunyho <andras.gunyho@aalto.fi>
2018-12-29 22:06:50 +0200

    Implement semitone mode.

--- Little-Utils/src/BiasKnobs.cpp
+++ Little-Utils/src/BiasKnobs.cpp
@@ -1,12 +1,17 @@
 #include "LittleUtils.hpp"
 #include "dsp/digital.hpp"
+#include "Util.hpp"
 #include "Widgets.hpp"
 
 #include <algorithm> // std::replace
 
+//TODO: consider 4 instead of 5.
 const int N_KNOBS = 5;
 
+const int MAX_SEMITONES = 36;
+
 //TODO: good name?
+//alternatives: offset/semitone, bias/semitone
 struct BiasKnobs : Module {
 	enum ParamIds {
 		BIAS_1_PARAM,
@@ -15,6 +20,8 @@ struct BiasKnobs : Module {
 		BIAS_4_PARAM,
 		BIAS_5_PARAM,
 		//BIAS_6_PARAM,
+		MODE_PARAM,
+		ONE_TO_MANY_PARAM,
 		NUM_PARAMS
 	};
 	enum InputIds {
@@ -63,7 +70,15 @@ void BiasKnobs::step() {
 
 	for(int i = 0; i < N_KNOBS; i++) {
 		float bias = params[BIAS_1_PARAM + i].value;
-		outputs[OUTPUT_1 + i].value = inputs[INPUT_1 + i].value + bias;
+		if(params[MODE_PARAM].value < 0.5f) {
+			// shift input CV by semitones
+			int st = bias * MAX_SEMITONES;
+			//outputs[OUTPUT_1 + i].value = inputs[INPUT_1 + i].value * powf(powf(2, 1.f/12.f), st);
+			outputs[OUTPUT_1 + i].value = inputs[INPUT_1 + i].value + st / 12.f;
+		} else {
+			// output volts
+			outputs[OUTPUT_1 + i].value = inputs[INPUT_1 + i].value + bias * 10.f;
+		}
 	}
 
 	//lights[GATE_LIGHT].setBrightnessSmooth(outputs[GATE_OUTPUT].value);
@@ -83,12 +98,12 @@ struct BiasKnobsWidget : ModuleWidget {
 		addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 2*RACK_GRID_WIDTH, 0)));
 		addChild(Widget::create<ScrewSilver>(Vec(box.size.x - 2*RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-		float elem_height = 65.f;
+		float elem_height = 58.f;
 		for(int i = 0; i < N_KNOBS; i++) {
 			float top_items_center_y = 30.f + i * elem_height;
-			float bot_items_center_y = 60.f + i * elem_height;
+			float bot_items_center_y = 55.f + i * elem_height;
 			addParam(createParamCentered<Trimpot>(Vec(20., top_items_center_y),
-						module, BiasKnobs::BIAS_1_PARAM + i, -10.f, 10.f, 0.f));
+						module, BiasKnobs::BIAS_1_PARAM + i, -1.f, 1.f, 0.f));
 			addInput(createInputCentered<PJ301MPort>(Vec(20., bot_items_center_y),
 						module, BiasKnobs::INPUT_1 + i));
 			//addOutput(createOutputCentered<PJ301MPort>(Vec(120 - 22.5, bot_items_center_y),
@@ -98,15 +113,23 @@ struct BiasKnobsWidget : ModuleWidget {
 
 			//TODO: is 'new' good? does it get freed somewhere?
 			TextBox *display = new TextBox();
-			display->box.pos = Vec(35.0, top_items_center_y - display->box.size.y * 0.5f);
 			//display->font_size = 18;
 			//display->box.size.x = 45;
 			display->font_size = 14;
 			display->box.size.x = 35;
+			display->box.size.y = 14;
 			display->textOffset.x = display->box.size.x * 0.5f;
+			display->box.pos = Vec(35.0, top_items_center_y - display->box.size.y * 0.5f);
 			displays[i] = display;
 			addChild(displays[i]);
 		}
+
+		addParam(createParam<CKSS>(Vec(7.5, 315), module, BiasKnobs::MODE_PARAM,
+				0.f, 1.f, 1.f
+				));
+		//addParam(createParam<CKSS>(Vec(40, 315), module, BiasKnobs::ONE_TO_MANY_PARAM,
+		//		0.f, 1.f, 1.f
+		//		));
 
 	}
 
@@ -114,7 +137,13 @@ struct BiasKnobsWidget : ModuleWidget {
 		ModuleWidget::step();
 		for(int i = 0; i < N_KNOBS; i++) {
 			float bias = module->params[BiasKnobs::BIAS_1_PARAM + i].value;
-			std::string s = stringf(fabs(bias) < 9.95f ? "%+.1fV" : "%+.0f.V", bias);
+			std::string s;
+			if(module->params[BiasKnobs::MODE_PARAM].value < 0.5f) {
+				int st = bias * MAX_SEMITONES;
+				s = stringf("%+3dst", st);
+			} else {
+				s = stringf(fabs(bias) < 0.995f ? "%+.1fV" : "%+.0f.V", bias * 10.f);
+			}
 			//std::string s = stringf(fabs(bias) < 9.95f ? "%+.1f" : "%+.0f.", bias);
 			std::replace(s.begin(), s.end(), '0', 'O');
 			displays[i]->setText(s);
