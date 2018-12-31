e46c49dfa97149d2bc4f8d7e45ac8525bd498d57
Marton Gunyho <andras.gunyho@aalto.fi>
2018-12-31 16:34:06 +0200

    Implement LEDs and one-to-many output to bias knobs.

--- Little-Utils/src/BiasKnobs.cpp
+++ Little-Utils/src/BiasKnobs.cpp
@@ -7,6 +7,13 @@
 
 //TODO: consider 4 instead of 5.
 const int N_KNOBS = 5;
+constexpr float KNOB_COLORS[N_KNOBS][3] = {
+	{0.0f, 1.0f, 0.0f},
+	{1.0f, 0.5f, 0.0f},
+	{1.0f, 0.0f, 0.0f},
+	{1.0f, 0.0f, 1.0f},
+	{0.0f, 0.0f, 1.0f} //TODO: not very visible on black
+};
 
 const int MAX_SEMITONES = 36;
 
@@ -21,7 +28,7 @@ struct BiasKnobs : Module {
 		BIAS_5_PARAM,
 		//BIAS_6_PARAM,
 		MODE_PARAM,
-		ONE_TO_MANY_PARAM,
+		//ONE_TO_MANY_PARAM,
 		NUM_PARAMS
 	};
 	enum InputIds {
@@ -43,9 +50,49 @@ struct BiasKnobs : Module {
 		NUM_OUTPUTS
 	};
 	enum LightIds {
+		INPUT_1_LIGHTR,
+		INPUT_1_LIGHTG,
+		INPUT_1_LIGHTB,
+		INPUT_2_LIGHTR,
+		INPUT_2_LIGHTG,
+		INPUT_2_LIGHTB,
+		INPUT_3_LIGHTR,
+		INPUT_3_LIGHTG,
+		INPUT_3_LIGHTB,
+		INPUT_4_LIGHTR,
+		INPUT_4_LIGHTG,
+		INPUT_4_LIGHTB,
+		INPUT_5_LIGHTR,
+		INPUT_5_LIGHTG,
+		INPUT_5_LIGHTB,
+		//INPUT_6_LIGHTR,
+		//INPUT_6_LIGHTG,
+		//INPUT_6_LIGHTB,
+
+		OUTPUT_1_LIGHTR,
+		OUTPUT_1_LIGHTG,
+		OUTPUT_1_LIGHTB,
+		OUTPUT_2_LIGHTR,
+		OUTPUT_2_LIGHTG,
+		OUTPUT_2_LIGHTB,
+		OUTPUT_3_LIGHTR,
+		OUTPUT_3_LIGHTG,
+		OUTPUT_3_LIGHTB,
+		OUTPUT_4_LIGHTR,
+		OUTPUT_4_LIGHTG,
+		OUTPUT_4_LIGHTB,
+		OUTPUT_5_LIGHTR,
+		OUTPUT_5_LIGHTG,
+		OUTPUT_5_LIGHTB,
+		//OUTPUT_6_LIGHTR,
+		//OUTPUT_6_LIGHTG,
+		//OUTPUT_6_LIGHTB,
+		//ONE_TO_MANY_LIGHT,
 		NUM_LIGHTS
 	};
 
+	//SchmittTrigger oneToManyTrigger;
+	//bool oneToManyState;
 	BiasKnobs() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {}
 
 	void step() override;
@@ -67,21 +114,35 @@ struct BiasKnobs : Module {
 };
 
 void BiasKnobs::step() {
+	//float deltaTime = engineGetSampleTime();
 
+	int li = 0; // index of the latest encountered active input
 	for(int i = 0; i < N_KNOBS; i++) {
 		float bias = params[BIAS_1_PARAM + i].value;
+		li = inputs[INPUT_1 + i].active ? i : li;
 		if(params[MODE_PARAM].value < 0.5f) {
 			// shift input CV by semitones
-			int st = bias * MAX_SEMITONES;
-			//outputs[OUTPUT_1 + i].value = inputs[INPUT_1 + i].value * powf(powf(2, 1.f/12.f), st);
-			outputs[OUTPUT_1 + i].value = inputs[INPUT_1 + i].value + st / 12.f;
+			bias = int(bias * MAX_SEMITONES) / 12.f;
 		} else {
 			// output volts
-			outputs[OUTPUT_1 + i].value = inputs[INPUT_1 + i].value + bias * 10.f;
+			bias *= 10.f;
 		}
+		outputs[OUTPUT_1 + i].value = inputs[INPUT_1 + li].value + bias;
+
+		// use setBrigthness instead of setBrightnessSmooth to reduce power usage
+		lights[INPUT_1_LIGHTR + 3*i].setBrightness(KNOB_COLORS[i][0]);
+		lights[INPUT_1_LIGHTG + 3*i].setBrightness(KNOB_COLORS[i][1]);
+		lights[INPUT_1_LIGHTB + 3*i].setBrightness(KNOB_COLORS[i][2]);
+
+		lights[OUTPUT_1_LIGHTR + 3*i].setBrightness(KNOB_COLORS[li][0]);
+		lights[OUTPUT_1_LIGHTG + 3*i].setBrightness(KNOB_COLORS[li][1]);
+		lights[OUTPUT_1_LIGHTB + 3*i].setBrightness(KNOB_COLORS[li][2]);
 	}
 
 	//lights[GATE_LIGHT].setBrightnessSmooth(outputs[GATE_OUTPUT].value);
+
+	//oneToManyTrigger.process(deltaTime);
+	//lights[ONE_TO_MANY_LIGHT].setBrightnessSmooth(params[ONE_TO_MANY_PARAM].value);
 
 }
 
@@ -104,12 +165,17 @@ struct BiasKnobsWidget : ModuleWidget {
 			float bot_items_center_y = 55.f + i * elem_height;
 			addParam(createParamCentered<Trimpot>(Vec(20., top_items_center_y),
 						module, BiasKnobs::BIAS_1_PARAM + i, -1.f, 1.f, 0.f));
-			addInput(createInputCentered<PJ301MPort>(Vec(20., bot_items_center_y),
+			Vec input_pos = Vec(20., bot_items_center_y);
+			addInput(createInputCentered<PJ301MPort>(input_pos,
 						module, BiasKnobs::INPUT_1 + i));
 			//addOutput(createOutputCentered<PJ301MPort>(Vec(120 - 22.5, bot_items_center_y),
 			//			module, BiasKnobs::OUTPUT_1 + i));
-			addOutput(createOutputCentered<PJ301MPort>(Vec(75 - 20., bot_items_center_y),
+			Vec output_pos = Vec(75 - 20., bot_items_center_y);
+			addOutput(createOutputCentered<PJ301MPort>(output_pos,
 						module, BiasKnobs::OUTPUT_1 + i));
+
+			addChild(createTinyLightForPort<RGBLight>(input_pos, module, BiasKnobs::INPUT_1_LIGHTR + 3*i));
+			addChild(createTinyLightForPort<RGBLight>(output_pos, module, BiasKnobs::OUTPUT_1_LIGHTR + 3*i));
 
 			//TODO: is 'new' good? does it get freed somewhere?
 			TextBox *display = new TextBox();
@@ -124,12 +190,16 @@ struct BiasKnobsWidget : ModuleWidget {
 			addChild(displays[i]);
 		}
 
-		addParam(createParam<CKSS>(Vec(7.5, 315), module, BiasKnobs::MODE_PARAM,
+		addParam(createParam<CKSS>(Vec(15, 311), module, BiasKnobs::MODE_PARAM,
 				0.f, 1.f, 1.f
 				));
 		//addParam(createParam<CKSS>(Vec(40, 315), module, BiasKnobs::ONE_TO_MANY_PARAM,
 		//		0.f, 1.f, 1.f
 		//		));
+		//addParam(createParam<ToggleLEDButton>(Vec(40, 315), module, BiasKnobs::ONE_TO_MANY_PARAM,
+		//		0.f, 1.f, 0.f
+		//		));
+		//addChild(ModuleLightWidget::create<MediumLight<GreenLight>>(Vec(37.5f + 7.f, 315+4.4f), module, BiasKnobs::ONE_TO_MANY_LIGHT));
 
 	}
 
