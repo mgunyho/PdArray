37ab7684cfdef074a69b708a7d73a2fe4d27fee8
mgunyho <marci.gunyho@gmail.com>
2019-08-14 14:36:53 +0300

    Add basic polyphony to MulDiv.

--- Little-Utils/src/MulDiv.cpp
+++ Little-Utils/src/MulDiv.cpp
@@ -3,6 +3,8 @@
 
 #include <algorithm> // std::replace
 
+const int MAX_POLY_CHANNELS = 16;
+
 struct MulDiv : Module {
 	enum ParamIds {
 		A_SCALE_PARAM,
@@ -27,14 +29,17 @@ struct MulDiv : Module {
 	};
 
 	// output this instead of NaN (when e.g. dividing by zero)
-	float valid_div_value = 0.f;
+	float valid_div_value[MAX_POLY_CHANNELS];
 
 	MulDiv() {
 		config(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS);
 		configParam(MulDiv::A_SCALE_PARAM,   0.f, 2.f, 0.f, "A scale");
 		configParam(MulDiv::B_SCALE_PARAM,   0.f, 2.f, 0.f, "B scale");
 		configParam(MulDiv::OUT_SCALE_PARAM, 0.f, 2.f, 0.f, "Output scale");
-		configParam(MulDiv::CLIP_ENABLE_PARAM, 0.f, 1.f, 0.f, "Clip");
+		configParam(MulDiv::CLIP_ENABLE_PARAM, 0.f, 1.f, 0.f, "Clip to +/-10V");
+		for(int i = 0; i < MAX_POLY_CHANNELS; i++) {
+			valid_div_value[i] = 0.f;
+		}
 	}
 
 	void process(const ProcessArgs &args) override;
@@ -52,23 +57,35 @@ void MulDiv::process(const ProcessArgs &args) {
 	bool clip = params[CLIP_ENABLE_PARAM].getValue() > 0.5f;
 	auto a_in = inputs[A_INPUT];
 	auto b_in = inputs[B_INPUT];
+	int ac = a_in.getChannels();
+	int bc = b_in.getChannels();
+	const int channels = std::max(ac, bc);
+
+	outputs[MUL_OUTPUT].setChannels(channels);
+	outputs[DIV_OUTPUT].setChannels(channels);
 
 	float as = int(params[A_SCALE_PARAM].getValue()) == 0 ? 1.0 : 1./(params[A_SCALE_PARAM].getValue() * 5.0);
 	float bs = int(params[B_SCALE_PARAM].getValue()) == 0 ? 1.0 : 1./(params[B_SCALE_PARAM].getValue() * 5.0);
 	float os = int(params[OUT_SCALE_PARAM].getValue()) == 0 ? 1.0 : params[OUT_SCALE_PARAM].getValue() * 5.0;
 
-	float m = (a_in.active ? a_in.value * as : 1.f) * (b_in.active ? b_in.value * bs : 1.f);
+	for(int c = 0; c < channels; c++) {
+
+	float a_cond = (c < ac) || ac == 1;
+	float b_cond = (c < bc) || bc == 1;
+	float m = (a_cond ? a_in.getPolyVoltage(c) * as : 1.f) * (b_cond ? b_in.getPolyVoltage(c) * bs : 1.f);
 	m *= os;
 
-	outputs[MUL_OUTPUT].setVoltage(clip ? clamp(m, -10.f, 10.f) : m);
+	outputs[MUL_OUTPUT].setVoltage(clip ? clamp(m, -10.f, 10.f) : m, c);
 
-	if(b_in.active) {
-		float d = (a_in.active ? a_in.value : 1.f) / b_in.value * os;
-		valid_div_value = std::isfinite(d) ? d : valid_div_value;
-		if(clip) valid_div_value = clamp(valid_div_value, -10.f, 10.f);
-		outputs[DIV_OUTPUT].setVoltage(valid_div_value);
+	if(b_cond) {
+		float d = (a_cond ? a_in.getPolyVoltage(c) : 1.f) / b_in.getPolyVoltage(c) * os;
+		valid_div_value[c] = std::isfinite(d) ? d : valid_div_value[c];
+		if(clip) valid_div_value[c] = clamp(valid_div_value[c], -10.f, 10.f);
+		outputs[DIV_OUTPUT].setVoltage(valid_div_value[c], c);
 	} else {
-		outputs[DIV_OUTPUT].setVoltage(clip ? clamp(a_in.value * os, -10.f, 10.f) : a_in.value * os);
+		outputs[DIV_OUTPUT].setVoltage(clip ? clamp(a_in.getPolyVoltage(c) * os, -10.f, 10.f) : a_in.getPolyVoltage(c) * os, c);
+	}
+
 	}
 
 	lights[CLIP_ENABLE_LIGHT].setSmoothBrightness(clip, deltaTime);
